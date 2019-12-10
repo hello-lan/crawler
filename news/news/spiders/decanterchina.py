7171095d0eeb1deeb52da409ac47d8ce57382dd8
lhq <haiqiang.lan@andui.com>
2019-12-10 09:37:49 +0800

    news 添加decanterchina新闻爬虫

---
+++ news/news/spiders/decanterchina.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.http import Request
+
+from itertools import chain
+from datetime import datetime
+
+from news.items import NewsItem
+
+
+class DecanterchinaSpider(scrapy.Spider):
+    name = 'decanterchina'
+    allowed_domains = ['decanterchina.com']
+    start_urls = ['https://www.decanterchina.com/zh/%E8%A1%8C%E4%B8%9A%E6%96%B0%E9%97%BB/']
+
+    custom_settings = {
+        "ITEM_PIPELINES": {
+            "news.pipelines.MongoDBPipeline": 301,
+        },
+        "DOWNLOAD_DELAY": 0.8,
+    }
+
+    def parse(self, response):
+        urls_1 = response.css(".featured h1 > a::attr(href)").getall()
+        urls_2 = response.css(".featured-sub-item h2 > a::attr(href)").getall()
+        urls_3 = response.css(".featured-list-item h3.medium > a::attr(href)").getall()
+        for url in chain(urls_1, urls_2, urls_3):
+            url = response.urljoin(url)
+            yield Request(url, callback=self.parse_item)
+
+        for next_url in response.css(".pagination > ul > li > a::attr(href)").getall():
+            next_url = response.urljoin(next_url)
+            yield Request(next_url)
+
+    def parse_item(self, response):
+        item = NewsItem()
+        item["url"] = response.url
+        item["title"] = response.css("header > h1::text").get('').strip()
+        item["author"] = response.css(".author > [rel=author]::attr(title)").get()
+        item["pub_time"] = response.css("time::attr(datetime)").get()
+        item["content"] = "".join(response.css(".article-content>p *::text").getall())
+        item["crawl_time"] = datetime.now()
+        return item
